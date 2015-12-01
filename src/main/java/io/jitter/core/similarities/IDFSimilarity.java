3acd5936e3373f42b0fd427116f5e7a8c215293f
Flavio Martins <flaviomartins@acm.org>
2015-12-01 04:22:33 +0000

    similarity test

--- src/main/java/io/jitter/core/similarities/IDFSimilarity.java
+++ src/main/java/io/jitter/core/similarities/IDFSimilarity.java
@@ -6,18 +6,8 @@ import org.apache.lucene.search.similarities.DefaultSimilarity;
 public class IDFSimilarity extends DefaultSimilarity {
 
     @Override
-    public float lengthNorm(FieldInvertState state) {
-        final int numTerms;
-        if (discountOverlaps)
-            numTerms = state.getLength() - state.getNumOverlap();
-        else
-            numTerms = state.getLength();
-        return state.getBoost() * (numTerms > 0 ? 1.0f : 0.0f);
-    }
-
-    @Override
     public float tf(float freq) {
-        return freq > 0 ? 1.0f : 0.0f;
+        return freq < 1 ? 0 : 1.0f;
     }
 
     @Override
