0e7f51fcea1b51719009e4763c2968cf82042523
Flavio Martins <flaviomartins@acm.org>
2016-03-21 00:53:05 +0100

    only build DocVectors for final searchers

---
+++ src/main/java/io/jitter/core/filter/MaxTFFilter.java
@@ -0,0 +1,41 @@
+package io.jitter.core.filter;
+
+import io.jitter.api.search.Document;
+import io.jitter.core.document.DocVector;
+
+import java.util.*;
+
+public class MaxTFFilter extends SearchFilter {
+    private final int maxTF;
+
+    public MaxTFFilter(int maxTF) {
+        this.maxTF = maxTF;
+    }
+
+    public void setResults(List<Document> results) {
+        this.results = results;
+        this.filter();
+    }
+
+    protected void filter() {
+        Iterator<Document> resultIt = results.iterator();
+
+        List<Document> updatedResults = new ArrayList<>();
+        while (resultIt.hasNext()) {
+            Document origResult = resultIt.next();
+
+            DocVector docVector = origResult.getDocVector();
+            if (docVector != null) {
+                Collection<Integer> freqs = docVector.vector.values();
+                Integer maxFreq = Collections.max(freqs);
+                if (maxFreq >= maxTF) {
+                    continue;
+                }
+            }
+
+            updatedResults.add(origResult);
+        }
+        results = updatedResults;
+    }
+
+}
--- src/main/java/io/jitter/resources/MultiFeedbackResource.java
+++ src/main/java/io/jitter/resources/MultiFeedbackResource.java
@@ -11,6 +11,7 @@ import io.dropwizard.jersey.params.BooleanParam;
 import io.dropwizard.jersey.params.IntParam;
 import io.jitter.api.search.SelectionFeedbackDocumentsResponse;
 import io.jitter.core.analysis.StopperTweetAnalyzer;
+import io.jitter.core.filter.MaxTFFilter;
 import io.jitter.core.filter.NaiveLanguageFilter;
 import io.jitter.core.search.TopDocuments;
 import io.jitter.core.selection.SelectionTopDocuments;
@@ -212,6 +213,10 @@ public class MultiFeedbackResource {
         NaiveLanguageFilter langFilter = new NaiveLanguageFilter("en");
         langFilter.setResults(results.scoreDocs);
         results.scoreDocs = langFilter.getFiltered();
+
+        MaxTFFilter maxTFFilter = new MaxTFFilter(3);
+        maxTFFilter.setResults(results.scoreDocs);
+        results.scoreDocs = maxTFFilter.getFiltered();
 
         long endTime = System.currentTimeMillis();
 
