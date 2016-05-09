87530cc4d22064893c3e5d3b5a0ba468bcb14df5
Flavio Martins <flaviomartins@acm.org>
2016-05-09 14:19:13 +0100

    RM3 idf fix: lowercase with analyzer and use collectionStats to weight expansion terms

--- src/main/java/io/jitter/core/feedback/FeedbackRelevanceModel.java
+++ src/main/java/io/jitter/core/feedback/FeedbackRelevanceModel.java
@@ -1,9 +1,12 @@
 package io.jitter.core.feedback;
 
+import io.jitter.api.collectionstatistics.CollectionStats;
 import io.jitter.api.search.Document;
 import io.jitter.core.document.FeatureVector;
+import io.jitter.core.twittertools.api.TrecCollectionStats;
 import io.jitter.core.utils.Stopper;
 import io.jitter.core.utils.KeyValuePair;
+import jsat.text.stemming.PorterStemmer;
 
 import java.util.*;
 
@@ -12,8 +15,14 @@ public class FeedbackRelevanceModel extends FeedbackModel {
     private boolean stripNumbers = false;
     private double[] docWeights = null;
 
+    private PorterStemmer stemmer = new PorterStemmer();
+
     @Override
     public void build(Stopper stopper) {
+        this.build(stopper, null);
+    }
+
+    public void build(Stopper stopper, CollectionStats collectionStats) {
         this.stopper = stopper;
         try {
             Set<String> vocab = new HashSet<>();
@@ -31,14 +40,12 @@ public class FeedbackRelevanceModel extends FeedbackModel {
             hitIterator = relDocs.iterator();
             while (hitIterator.hasNext()) {
                 Document hit = hitIterator.next();
-                String text = hit.getText().toLowerCase(Locale.ROOT);
-                FeatureVector docVector = new FeatureVector(text, stopper);
+                FeatureVector docVector = new FeatureVector(hit.getText(), stopper);
                 vocab.addAll(docVector.getFeatures());
                 fbDocVectors.add(docVector);
             }
 
             features = new LinkedList<>();
-
 
             for (String term : vocab) {
                 double fbWeight = 0.0;
@@ -56,6 +63,12 @@ public class FeedbackRelevanceModel extends FeedbackModel {
                     docProb *= scores[k++] * docWeight;
 
                     fbWeight += docProb;
+                }
+
+                if (collectionStats != null) {
+                    String stem = stemmer.stem(term);
+                    double rm3IDF = TrecCollectionStats.DEFAULT_TERMS_SIZE / (double) collectionStats.getDF(stem);
+                    fbWeight *= Math.log(rm3IDF);
                 }
 
                 fbWeight /= (double) fbDocVectors.size();
