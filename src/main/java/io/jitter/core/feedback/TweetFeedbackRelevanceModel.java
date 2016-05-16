efbbc88f51386af904d33595483128c2ddc761f1
Flavio Martins <flaviomartins@acm.org>
2016-05-16 12:18:20 +0100

    ASCII-only minWordLen=3 minTermFreq=1

--- src/main/java/io/jitter/core/feedback/TweetFeedbackRelevanceModel.java
+++ src/main/java/io/jitter/core/feedback/TweetFeedbackRelevanceModel.java
@@ -1,13 +1,15 @@
 package io.jitter.core.feedback;
 
+import com.google.common.base.CharMatcher;
 import org.apache.lucene.analysis.Analyzer;
 
 public class TweetFeedbackRelevanceModel extends FeedbackRelevanceModel {
 
     public TweetFeedbackRelevanceModel(Analyzer analyzer) {
         super(analyzer);
-        setMinWordLen(1);
+        setMinWordLen(3);
         setMinDocFreq(10);
+        setMinTermFreq(1);
     }
 
     @Override
@@ -15,7 +17,10 @@ public class TweetFeedbackRelevanceModel extends FeedbackRelevanceModel {
         if (super.isNoiseWord(term)) {
             return true;
         }
-        return (term.startsWith("@"));
+        if (term.startsWith("@")) {
+            return true;
+        }
+        return (!CharMatcher.ASCII.matchesAllOf(term));
     }
 
 }
