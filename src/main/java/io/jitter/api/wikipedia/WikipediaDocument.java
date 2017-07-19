53b55e189e54473219f10c3c0133e848d7121e0f
Flavio Martins <flaviomartins@acm.org>
2017-07-19 02:10:43 +0100

    add simple StoredField for categories

--- src/main/java/io/jitter/api/wikipedia/WikipediaDocument.java
+++ src/main/java/io/jitter/api/wikipedia/WikipediaDocument.java
@@ -12,18 +12,24 @@ public class WikipediaDocument extends AbstractDocument implements Document {
     public double rsv; // required
     public String title; // required
     public String text; // required
+    public String date; // required
+    public String[] categories;
 
     public WikipediaDocument(WikipediaDocument other) {
         id = other.id;
         rsv = other.rsv;
         title = other.title;
         text = other.text;
+        date = other.date;
+        categories = other.categories;
     }
 
     public WikipediaDocument(org.apache.lucene.document.Document hit) {
         this.id = hit.get(WikipediaManager.ID_FIELD);
         this.title = hit.get(WikipediaManager.TITLE_FIELD);
         this.text = hit.get(WikipediaManager.TEXT_FIELD);
+        this.date = hit.get(WikipediaManager.DATE_FIELD);
+        this.categories = hit.getValues(WikipediaManager.CATEGORIES_FIELD);
     }
 
     @Override
@@ -60,5 +66,21 @@ public class WikipediaDocument extends AbstractDocument implements Document {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String[] getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String[] categories) {
+        this.categories = categories;
     }
 }
