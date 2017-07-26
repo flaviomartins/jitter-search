1445b8c83fec56f5a86f42e4e15b49e0cd72c318
Flavio Martins <flaviomartins@acm.org>
2017-07-26 23:17:16 +0100

    use @NotEmpty to validate query argument

--- src/main/java/io/jitter/resources/TrecRMTSResource.java
+++ src/main/java/io/jitter/resources/TrecRMTSResource.java
@@ -25,6 +25,7 @@ import io.jitter.core.utils.Epochs;
 import io.swagger.annotations.*;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.thrift.TException;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -76,7 +77,7 @@ public class TrecRMTSResource extends AbstractFeedbackResource {
             @ApiResponse(code = 404, message = "No results found"),
             @ApiResponse(code = 500, message = "Internal Server Error")
     })
-    public SelectionSearchResponse search(@ApiParam(value = "Search query", required = true) @QueryParam("q") Optional<String> q,
+    public SelectionSearchResponse search(@ApiParam(value = "Search query", required = true) @QueryParam("q") @NotEmpty String q,
                                           @ApiParam(hidden = true) @QueryParam("fq") Optional<String> fq,
                                           @ApiParam(value = "Limit results", allowableValues="range[1, 10000]") @QueryParam("limit") @DefaultValue("1000") Integer limit,
                                           @ApiParam(value = "Include retweets") @QueryParam("retweets") @DefaultValue("false") Boolean retweets,
@@ -100,13 +101,9 @@ public class TrecRMTSResource extends AbstractFeedbackResource {
                                           @ApiParam(hidden = true) @Context UriInfo uriInfo) {
         MultivaluedMap<String, String> params = uriInfo.getQueryParameters();
 
-        if (!q.isPresent() || q.get().isEmpty()) {
-            throw new BadRequestException();
-        }
-
         try {
             long startTime = System.currentTimeMillis();
-            String query = URLDecoder.decode(q.orElse(""), "UTF-8");
+            String query = URLDecoder.decode(q, "UTF-8");
             long[] epochs = Epochs.parseEpoch(epoch);
 
             Selection selection;
