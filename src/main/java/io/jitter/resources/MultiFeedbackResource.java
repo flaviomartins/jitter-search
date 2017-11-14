d206d4cf696d86aa07a62e7937776dd256f5c101
Flavio Martins <flaviomartins@acm.org>
2017-11-14 16:19:16 -0500

    add rerank flag

--- src/main/java/io/jitter/resources/MultiFeedbackResource.java
+++ src/main/java/io/jitter/resources/MultiFeedbackResource.java
@@ -100,6 +100,7 @@ public class MultiFeedbackResource extends AbstractFeedbackResource {
                                           @ApiParam(hidden = true) @QueryParam("fbMerge") @DefaultValue("false") Boolean fbMerge,
                                           @ApiParam(value = "Use topics") @QueryParam("topics") @DefaultValue("true") Boolean topics,
                                           @ApiParam(value = "Use temporal reranking") @QueryParam("temporal") @DefaultValue("false") Boolean temporal,
+                                          @ApiParam(hidden = true) @QueryParam("rerank") @DefaultValue("true") Boolean rerank,
                                           @ApiParam(hidden = true) @Context UriInfo uriInfo) {
         MultivaluedMap<String, String> params = uriInfo.getQueryParameters();
 
@@ -167,7 +168,7 @@ public class MultiFeedbackResource extends AbstractFeedbackResource {
 
             RerankerCascade cascade = new RerankerCascade();
             if (temporal) {
-                cascade.add(new RMTSReranker("mf.model", query, queryEpoch, (List<StatusDocument>) shardResults.scoreDocs, searchManager.getCollectionStats(), limit, limit, false));
+                cascade.add(new RMTSReranker("mf.model", query, queryEpoch, (List<StatusDocument>) shardResults.scoreDocs, searchManager.getCollectionStats(), limit, limit, rerank));
             }
 //            cascade.add(new MaxTFFilter(5));
 
