7f4fb73d74b7e31d907bf017529756e4a9b2d694
Flavio Martins <flaviomartins@acm.org>
2019-02-06 00:23:07 +0000

    Switch home timeline to polling since streaming is gone.

--- src/main/java/io/jitter/core/hbc/twitter4j/RawTwitter4jUserstreamClient.java
+++ src/main/java/io/jitter/core/hbc/twitter4j/RawTwitter4jUserstreamClient.java
@@ -2,8 +2,6 @@ package io.jitter.core.hbc.twitter4j;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import com.twitter.hbc.core.Client;
-import com.twitter.hbc.twitter4j.Twitter4jUserstreamClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import twitter4j.*;
@@ -12,20 +10,20 @@ import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 
-public class RawTwitter4jUserstreamClient extends Twitter4jUserstreamClient {
+public class RawTwitter4jUserstreamClient {
 
     private final static Logger logger = LoggerFactory.getLogger(RawTwitter4jUserstreamClient.class);
 
-    private final Client client;
     private final BlockingQueue<String> messageQueue;
     private final ExecutorService executorService;
+    private final List<UserStreamListener> listeners;
     private final List<RawStreamListener> rawStreamListeners;
 
-    public RawTwitter4jUserstreamClient(Client client, BlockingQueue<String> blockingQueue, List<UserStreamListener> listeners, ExecutorService executorService, List<RawStreamListener> rawStreamListeners) {
-        super(client, blockingQueue, listeners, executorService);
-        this.client = Preconditions.checkNotNull(client);
+    public RawTwitter4jUserstreamClient(BlockingQueue<String> blockingQueue, List<UserStreamListener> listeners, ExecutorService executorService, List<RawStreamListener> rawStreamListeners) {
         this.messageQueue = Preconditions.checkNotNull(blockingQueue);
         this.executorService = Preconditions.checkNotNull(executorService);
+        Preconditions.checkNotNull(listeners);
+        this.listeners = ImmutableList.copyOf(listeners);
         Preconditions.checkNotNull(rawStreamListeners);
         this.rawStreamListeners = ImmutableList.copyOf(rawStreamListeners);
     }
@@ -34,18 +32,18 @@ public class RawTwitter4jUserstreamClient extends Twitter4jUserstreamClient {
      * Forks off a runnable with the executor provided. Multiple calls are allowed, but the listeners must be
      * threadsafe.
      */
-    @Override
     public void process() {
-        if (client.isDone() || executorService.isTerminated()) {
+        if (executorService.isTerminated()) {
             throw new IllegalStateException("Client is already stopped");
         }
         Runnable runner = () -> {
             try {
-                while (!client.isDone()) {
+                while (true) {
                     String msg = messageQueue.take();
+                    Status status = TwitterObjectFactory.createStatus(msg);
                     try {
                         onMessage(msg);
-                        parseMessage(msg);
+                        onStatus(status);
                     } catch (Exception e) {
                         logger.warn("Exception thrown during parsing msg " + msg, e);
                         onException(e);
@@ -62,6 +60,18 @@ public class RawTwitter4jUserstreamClient extends Twitter4jUserstreamClient {
     private void onMessage(String rawString) {
         for (RawStreamListener listener : rawStreamListeners) {
             listener.onMessage(rawString);
+        }
+    }
+
+    protected void onStatus(final Status status) {
+        for (UserStreamListener listener : listeners) {
+            listener.onStatus(status);
+        }
+    }
+
+    private void onException(Exception e) {
+        for (UserStreamListener listener : listeners) {
+            listener.onException(e);
         }
     }
 
